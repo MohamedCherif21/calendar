e0645d764fe280d369def29cd885ad70d89893f2
jerbii <cherif.mohamed@esprit.tn>
2023-11-06 21:56:41 +0100

    commit 1

--- my-cv/src/App.js
+++ my-cv/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button, Space } from 'antd';
-import { BrowserRouter, Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import Home from './Pages/Home';
 import Profile from './Pages/Profile';
-import Templates from './Pages/templates';
 import ForgotPassword from './Pages/ForgotPassword';
 import ResetPassword from './Pages/ResetPassword';
 import Admin from './Pages/Admin';
@@ -26,7 +23,6 @@ function App() {
           <Route path='/' element={<ProtectedRoute><Home /></ProtectedRoute>} />
           <Route path='/home' element={<ProtectedRoute><Home /></ProtectedRoute>} />
           <Route path='/profile' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-          <Route path='/templates/:id' element={<ProtectedRoute><Templates /></ProtectedRoute>} />
           <Route path='/reset-password/:id' element={<ResetPassword />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path='/login' element={<Login />} />
